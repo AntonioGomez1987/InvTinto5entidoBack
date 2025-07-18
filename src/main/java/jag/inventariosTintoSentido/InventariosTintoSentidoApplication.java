769f463c36193ad6da9f40530a1648f70b13d2f6
Antonio Gomez <antogo8768@gmail.com>
2025-07-18 12:38:26 -0500

    inicio

---
+++ src/main/java/jag/inventariosTintoSentido/InventariosTintoSentidoApplication.java
@@ -0,0 +1,13 @@
+package jag.inventariosTintoSentido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariosTintoSentidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariosTintoSentidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jag/inventariosTintoSentido/InventariosTintoSentidoApplicationTests.java
@@ -0,0 +1,13 @@
+package jag.inventariosTintoSentido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariosTintoSentidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
